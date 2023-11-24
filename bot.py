22d6b1b1863713776e38bfbe6599321e30e64979
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2023-11-24 16:42:33 +0400

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,18 +1,54 @@
 import os
 
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 from dotenv import load_dotenv
-from telegram.ext import Updater, MessageHandler, Filters
 
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+load_dotenv()
+bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
+dp = Dispatcher()
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    from google.cloud import dialogflow
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
+
+
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nЯ твой бот помощник!\nЗадай мне вопрос')
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Помогу тебе с самыми важными вопросами'
+    )
+
+
+@dp.message()
+async def send_echo(message: Message):
+    user_message = message.text
+    chat_id = message.chat.id
+    bot_answer = detect_intent_texts('angular-yeti-403509', chat_id, user_message, 'ru')
+    await message.reply(text=bot_answer)
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    tg_token = os.getenv("TELEGRAM_BOT_TOKEN")
-    updater = Updater(tg_token)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
-    updater.start_polling()
-    updater.idle()
+    dp.run_polling(bot)
